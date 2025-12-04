94782de806529ca563299efce4af20250215f86b
DylanoNv <9015493@student.zadkine.nl>
2025-12-04 13:34:17 +0100

    opdracht 3

---
+++ opdracht 3/index.php
@@ -0,0 +1,13 @@
+<?php
+$voornaam = "Dylano";
+$achternaam = "Duif";
+
+echo "Voornaam: " . $voornaam . "<br>";
+echo "Achternaam: " . $achternaam . "<br><br>";
+
+$getal1 = 25;
+$getal2 = 90;
+
+print "Eerste getal: " . $getal1 . "<br>";
+print "Tweede getal: " . $getal2 . "<br>";
+?>
