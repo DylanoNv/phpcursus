3f741b125c500907baeb4cf43b0a4f85ffec19fd
DylanoNv <9015493@student.zadkine.nl>
2026-04-02 13:00:29 +0200

    CRUD TOETS
    nog een aanpassingen

---
+++ crud toets/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <?php
+    // functie: Programma CRUD bier
+    // auteur: Dylano Nietveld 
+
+    include 'functions.php';
+
+    crudMain();
+    ?>
+
+</body>
+</html>
