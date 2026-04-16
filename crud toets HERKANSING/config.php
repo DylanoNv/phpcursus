1785460c14c5e3c62be44c4f283309eb83023d77
DylanoNv <9015493@student.zadkine.nl>
2026-04-16 13:36:41 +0200

    crud toets bier copy paste

---
+++ crud toets HERKANSING/config.php
@@ -0,0 +1,12 @@
+<?php
+// auteur: Dylano Nietveld
+// functie: configuratiebestand
+
+define("DATABASE", "bieren");
+define("SERVERNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+
+define("CRUD_TABLE", "bier");
+
+?>
---
+++ crud toets HERKANSING/delete.php
@@ -0,0 +1,15 @@
+<?php
+// auteur: Dylano Nietveld
+// functie: verwijder een bier op basis van de id
+include 'functions.php';
+
+if(isset($_GET['id'])){
+
+    if(deleteRecord($_GET['id']) == true){
+        echo '<script>alert("Biercode: ' . $_GET['id'] . ' is verwijderd")</script>';
+        echo "<script> location.replace('index.php'); </script>";
+    } else {
+        echo '<script>alert("Bier is NIET verwijderd")</script>';
+    }
+}
+?>
