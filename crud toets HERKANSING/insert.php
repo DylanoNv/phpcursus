77e233b1518da044eda0e08b312ffc8d4a7910ec
DylanoNv <9015493@student.zadkine.nl>
2026-04-16 15:00:29 +0200

    laatste commit

--- crud toets HERKANSING/insert.php
+++ crud toets HERKANSING/insert.php
@@ -1,37 +1,39 @@
 <?php
-    // functie: formulier en database insert brouwer
-    // auteur: Dylano Nietveld
+// functie: formulier en database insert genre
+// auteur: Dylano Nietveld
 
-    echo "<h1>Insert Brouwer</h1>";
+echo "<h1>Insert Genre</h1>";
 
-    require_once('functions.php');
-	 
-    // Test of er op de insert-knop is gedrukt 
-    if(isset($_POST) && isset($_POST['btn_ins'])){
+require_once('functions.php');
 
-        // test of insert gelukt is
-        if(insertRecord($_POST) == true){
-            echo "<script>alert('Brouwer is toegevoegd')</script>";
-            echo "<script> location.replace('index.php'); </script>";
-        } else {
-            echo '<script>alert("Brouwer is NIET toegevoegd")</script>';
-        }
+if(isset($_POST) && isset($_POST['btn_ins'])){
+
+    if(insertRecord($_POST) == true){
+        echo "<script>alert('Genre is toegevoegd')</script>";
+        echo "<script> location.replace('index.php'); </script>";
+    } else {
+        echo '<script>alert("Genre is NIET toegevoegd")</script>';
     }
+}
 ?>
-<html>
-    <body>
-        <form method="post">
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insert Genre</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form method="post">
 
-        <label for="naam">Naam:</label>
-        <input type="text" id="naam" name="naam" required><br>
-
-        <label for="land">Land:</label>
-        <input type="text" id="land" name="land" required><br>
+        <label for="genrenaam">Genrenaam:</label>
+        <input type="text" id="genrenaam" name="genrenaam" required><br>
 
         <input type="submit" name="btn_ins" value="Insert">
-        </form>
-        
-        <br><br>
-        <a href='index.php'>Home</a>
-    </body>
+    </form>
+    
+    <br><br>
+    <a href='index.php'>Home</a>
+</body>
 </html>
